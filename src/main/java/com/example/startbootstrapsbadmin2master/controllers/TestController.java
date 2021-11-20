c5a08e4c310dd156d230d151cc3aa83c5d1ecf8b
adityashukla11 <shuklakiit@gmail.com>
2021-11-20 17:15:44 +0530

    new tables

--- src/main/java/com/example/startbootstrapsbadmin2master/controllers/TestController.java
+++ src/main/java/com/example/startbootstrapsbadmin2master/controllers/TestController.java
@@ -1,28 +1,36 @@
 package com.example.startbootstrapsbadmin2master.controllers;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class TestController {
 
 
-
-        @RequestMapping(value = "/index.html")
-        public String hello() {
-            return "index";
-        }
-    @RequestMapping(value = "/tables.html")
-    public String tables() {
+  @RequestMapping(value = "/index.html")
+  public String hello() {
+      return "index";
+    }
+  @RequestMapping(value = "/tables.html")
+  public String tables() {
         return "tables";
     }
-    @RequestMapping(value = "/charts.html")
-    public String charts(Model model)
-    {
-        model.addAttribute("text","controllertest");
+  @RequestMapping(value = "/tables1.html")
+  public String tables1() {
+        return "tables1";
+    }
+  
+  @RequestMapping(value = "/tables2.html")
+  public String tables2() {
+        return "tables2";
+    }
+
+  @RequestMapping(value = "/charts.html")
+  public String charts(Model model)
+   {
+       model.addAttribute("text","controllertest");
         //obj.sav(input);
         return "charts";
 
